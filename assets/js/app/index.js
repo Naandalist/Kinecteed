5c90442bd321727adc449b774cb0a1d5d9c181e2
“naandalist” <listiananda.apriliawan@gmail.com>
2024-04-08 07:49:29 +0700

    chore: code refactored

--- assets/js/app/index.js
+++ assets/js/app/index.js
@@ -24,17 +24,17 @@ function renderContacts() {
   let storedContacts = loadContactsFromLocalStorage();
   console.log(storedContacts);
 
-  const filterBy = params.get("filterBy");
+  const tag = params.get("tag");
 
   const filterConditions = {
     work: (item) => item.tag === "work",
     family: (item) => item.tag === "family",
     community: (item) => item.tag === "community",
-    favourites: (item) => item.isFavourite === true,
+    favorites: (item) => item.isFavourite === true,
   };
 
-  if (filterConditions[filterBy]) {
-    storedContacts = storedContacts.filter(filterConditions[filterBy]);
+  if (filterConditions[tag]) {
+    storedContacts = storedContacts.filter(filterConditions[tag]);
   }
 
   // its seeder, auto generate mock data 😎
@@ -319,7 +319,7 @@ function createTableRows(data) {
 
 function createButtonMenuSidebar() {
   const urlParams = new URLSearchParams(window.location.search);
-  const selectedCategory = urlParams.get("filterBy");
+  const selectedCategory = urlParams.get("tag");
 
   const isButtonMenuActive = (category) => {
     if (category === "allContact" && !selectedCategory) return "bg-blue-500/13";
@@ -341,14 +341,14 @@ function createButtonMenuSidebar() {
   </li>
   <li class="mt-0.5 w-full">
     <a class="py-2.7 ${isButtonMenuActive(
-      "favourites"
+      "favorites"
     )} text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-      href="/?filterBy=favourites">
+      href="/?tag=favorites">
       <div
         class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
         <i class="relative top-0 text-sm leading-normal text-red-500 ni ni-favourite-28"></i>
       </div>
-      <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Favourites</span>
+      <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">favorites</span>
     </a>
   </li>
   <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
@@ -361,7 +361,7 @@ function createButtonMenuSidebar() {
     <a class="py-2.7 ${isButtonMenuActive(
       "work"
     )} text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-      href="/?filterBy=work">
+      href="/?tag=work">
       <div
         class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
         <i class="relative top-0 text-sm leading-normal text-orange-500 ni ni-tag"></i>
@@ -373,7 +373,7 @@ function createButtonMenuSidebar() {
     <a class="py-2.7 ${isButtonMenuActive(
       "community"
     )} text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-      href="/?filterBy=community">
+      href="/?tag=community">
       <div
         class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
         <i class="relative top-0 text-sm leading-normal text-emerald-500 ni ni-tag"></i>
@@ -385,7 +385,7 @@ function createButtonMenuSidebar() {
     <a class="py-2.7 ${isButtonMenuActive(
       "family"
     )} text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-      href="/?filterBy=family">
+      href="/?tag=family">
       <div
         class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
         <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-tag"></i>
